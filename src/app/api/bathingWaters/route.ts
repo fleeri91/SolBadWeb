e69c788de57f14b91620d6f8cc0af0b14813aaf0
Flemming <fleeri91@gmail.com>
2025-05-21 01:27:52 +0200

    bathing waters route

---
+++ src/app/api/bathingWaters/route.ts
@@ -0,0 +1,39 @@
+import { Bath } from '@/types/BathingWater';
+import {
+  HAVBathingWaterRoot,
+  WatersAndAdvisory,
+} from '@/types/HAV/HAVBathingWater';
+import axios from 'axios';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  try {
+    const response = await axios.get<HAVBathingWaterRoot>(
+      'https://gw-test.havochvatten.se/external-public/bathing-waters/v2/bathing-waters'
+    );
+
+    const data = response.data;
+
+    const baths: Bath[] = data.watersAndAdvisories
+      .filter(
+        (item): item is WatersAndAdvisory =>
+          !!item.bathingWater?.samplingPointPosition
+      )
+      .map((item) => {
+        const bathingWater = item.bathingWater;
+        return {
+          name: bathingWater.name,
+          coordinates: {
+            lon: Number(bathingWater.samplingPointPosition.longitude),
+            lat: Number(bathingWater.samplingPointPosition.latitude),
+          },
+          id: bathingWater.id,
+        };
+      });
+
+    return NextResponse.json(baths);
+  } catch (error) {
+    console.error('Failed to fetch bathing waters:', error);
+    return new NextResponse('Failed to fetch bath spots', { status: 500 });
+  }
+}
