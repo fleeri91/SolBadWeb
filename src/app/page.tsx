b8911136da139f7fa668bdd9bc10bef30c73718f
Flemming <fleeri91@gmail.com>
2025-05-22 00:32:07 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Map from '@/components/Map'
+import OnBoardingForm from '@/components/OnBoardingForm'
+import { Container, Flex } from '@mantine/core'
 
 const HomePage = async () => {
   return (
-    <div className="h-screen">
-      <Map />
+    <div className="h-screen bg-blue-400">
+      <Container size="md" className="h-full">
+        <Flex justify="center" align="center" className="h-full">
+          <OnBoardingForm />
+        </Flex>
+      </Container>
     </div>
   )
 }
